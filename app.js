1d82dc2749d92fa76a7b8e0912571c9670bb618d
lilianastrajescu <liliana.strajescu96@gmail.com>
2022-08-12 12:40:36 +0200

    Added new icons

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ function displayForecast(response) {
   let forecastHTML = ` <div class="row forecast">`;
   //loop
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7) {
       forecastHTML =
         forecastHTML +
         `<div class="col section-right">
