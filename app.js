df8e91ddcdf01632c0c6f12cfaf578a334064137
lilianastrajescu <liliana.strajescu96@gmail.com>
2022-08-12 11:52:07 +0200

    Added current position button

--- app.js
+++ app.js
@@ -165,19 +165,23 @@ form.addEventListener("submit", handleSubmit);
 search("Calgary");
 
 //My position 
-/*function myPosition(position) {
+
+function currentPosition() {
+  navigator.geolocation.getCurrentPosition(myPosition);;
+}
+
+function myPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "3d6bcb1e707f4511e0a24749086c8223";
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(url).then(weather);
 }
-navigator.geolocation.getCurrentPosition(myPosition);
 
--let button = document.querySelector(".button-position");
+
+let button = document.querySelector(".button-position");
 function loc(request) {
   request.preventDefault();
   navigator.geolocation.getCurrentPosition(myPosition);
 }
 button.addEventListener("click", loc);
-*/
