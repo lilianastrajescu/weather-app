01b7b17958a90815c7e1d697030be184e31569f8
lilianastrajescu <liliana.strajescu96@gmail.com>
2023-07-02 18:43:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ function displayForecast(response) {
 //Geo Location/weather
 function getForecast(coordinates) {
   let apiKey = "c1a0d13315368ccd854557894643f312";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function weather(response) {
